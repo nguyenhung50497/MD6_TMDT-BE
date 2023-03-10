2aea180e9b1b9a66c3933d3726d44b183443a1b2
hungnguyen50497 <nguyenhung50497@gmail.com>
2023-03-10 16:40:36 +0700

    tao model database

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,7 @@ export const AppDataSource = new DataSource({
  port: 3306,
  username: "root",
  password: "123456",
- database: "thuong-mai-dien-tu",
+ database: "tmdt",
  synchronize: true,
  logging: false,
  entities: ["dist/src/model/*.js"],
---
+++ src/model/user.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  idUser: number;
+  @Column({ default: "" })
+  username: string;
+  @Column({ default: "" })
+  password: string;
+  @Column({ default: "" })
+  emailUser: string;
+  @Column({ default: "" })
+  fullName: string;
+  @Column({ default: "" })
+  phoneUser: number;
+  @Column({ type: "text", default: "" })
+  avatar: string;
+  @Column({ default: "customer" })
+  role: string;
+}
