e51f9662f13ca8ee85c19576b7944903c5f497af
hungnguyen50497 <nguyenhung50497@gmail.com>
2023-03-17 12:02:07 +0700

    merge feature/cart

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,94 +1,99 @@
 import {Request, Response} from "express";
 import AuthService from "../service/AuthService";
+import CartService from "../service/CartService";
 
 class AuthController {
-    private AuthService
+   private AuthService;
+   private CartService;
 
-    constructor() {
-        this.AuthService = AuthService;
-    }
+   constructor() {
+      this.AuthService = AuthService;
+      this.CartService = CartService;
+   }
 
-    login = async (req: Request, res: Response) => {
-        try {
-            let user = await this.AuthService.checkUser(req.body)
-            res.status(200).json(user)
-        } catch (e) {
-            res.status(500).json(e.message)
-        }
-    }
-    checkEmail = async (req: Request, res: Response) => {
-        try {
-            let check = req.body
-            let email = check.emailUser
-            let checkEmail = await this.AuthService.checkEmail(email)
-            res.status(200).json(checkEmail)
-        } catch (e) {
-            res.status(500).json(e.message);
-        }
-    }
-    checkPhone = async (req: Request, res: Response) => {
-        try {
-            let check = await this.AuthService.checkPhoneUser(req.body)
-            res.status(200).json(check)
-        } catch (e) {
-            res.status(500).json(e.message);
-        }
-    }
-    register = async (req: Request, res: Response) => {
-        try {
-            let userRegister = req.body
-            let user = await this.AuthService.register(userRegister)
-            res.status(200).json(user)
-        } catch (e) {
-            res.status(500).json(e.message);
-        }
-    }
-    loginGoogle = async (req: Request, res: Response) => {
-        try {
-            let userGoogle = req.body
-            let checkEmail = await this.AuthService.checkEmail(userGoogle.email)
-            let userCheck = {username: '', idGoogle: ''}
-            if (checkEmail === 'not allowed') {
-                userCheck.username = userGoogle.email
-                userCheck.idGoogle = userGoogle.id
-            } else {
-                const baseString =
-                    "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+   login = async (req: Request, res: Response) => {
+      try {
+         let user = await this.AuthService.checkUser(req.body);
+         res.status(200).json(user);
+      } catch (e) {
+         res.status(500).json(e.message);
+      }
+   };
+   checkEmail = async (req: Request, res: Response) => {
+      try {
+         let check = req.body;
+         let email = check.emailUser;
+         let checkEmail = await this.AuthService.checkEmail(email);
+         res.status(200).json(checkEmail);
+      } catch (e) {
+         res.status(500).json(e.message);
+      }
+   };
+   checkPhone = async (req: Request, res: Response) => {
+      try {
+         let check = await this.AuthService.checkPhoneUser(req.body);
+         res.status(200).json(check);
+      } catch (e) {
+         res.status(500).json(e.message);
+      }
+   };
+   register = async (req: Request, res: Response) => {
+      try {
+         let userRegister = req.body;
+         let user = await this.AuthService.register(userRegister);
+         let cart = await this.CartService.saveCart({idUser: user.idUser});
+         res.status(200).json(user);
+      } catch (e) {
+         res.status(500).json(e.message);
+      }
+   };
+   loginGoogle = async (req: Request, res: Response) => {
+      try {
+         let userGoogle = req.body;
+         let checkEmail = await this.AuthService.checkEmail(userGoogle.email);
+         let userCheck = { username: "", idGoogle: "" };
+         if (checkEmail === "not allowed") {
+            userCheck.username = userGoogle.email;
+            userCheck.idGoogle = userGoogle.id;
+         } else {
+            const baseString =
+               "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
 
-                const getRandomInt = (min, max) => {
-                    return Math.floor(Math.random() * (max - min)) + min;
-                };
+            const getRandomInt = (min, max) => {
+               return Math.floor(Math.random() * (max - min)) + min;
+            };
 
-                const getRandomString = (length, base) => {
-                    let result = ''
-                    const baseLength = base.length;
+            const getRandomString = (length, base) => {
+               let result = "";
+               const baseLength = base.length;
 
-                    for (let i = 0; i < length; i++) {
-                        const randomIndex = getRandomInt(0, baseLength);
-                        result += base[randomIndex];
-                    }
+               for (let i = 0; i < length; i++) {
+                  const randomIndex = getRandomInt(0, baseLength);
+                  result += base[randomIndex];
+               }
 
-                    return result;
-                };
-                let passwordGoogle = getRandomString(8, baseString)
-                let googleRegister = {
-                    username: userGoogle.email,
-                    password: passwordGoogle,
-                    emailUser: userGoogle.email,
-                    fullName: userGoogle.given_name,
-                    avatar: userGoogle.picture,
-                    idGoogle: userGoogle.id,
-                }
-                await this.AuthService.registerGoogle(googleRegister)
-                userCheck.username = userGoogle.email
-                userCheck.idGoogle = userGoogle.id
-            }
-            let user = await this.AuthService.checkUserGoogle(userCheck)
-            res.status(200).json(user)
-        } catch (e) {
-            res.status(500).json(e.message);
-        }
-    }
+               return result;
+            };
+            let passwordGoogle = getRandomString(8, baseString);
+            let googleRegister = {
+               username: userGoogle.email,
+               password: passwordGoogle,
+               emailUser: userGoogle.email,
+               fullName: userGoogle.given_name,
+               avatar: userGoogle.picture,
+               idGoogle: userGoogle.id,
+            };
+            let user = await this.AuthService.registerGoogle(googleRegister);
+            let cart = await this.CartService.saveCart({ idUser: user.idUser });
+            userCheck.username = userGoogle.email;
+            userCheck.idGoogle = userGoogle.id;
+         }
+         let user = await this.AuthService.checkUserGoogle(userCheck);
+         res.status(200).json(user);
+      } catch (e) {
+         res.status(500).json(e.message);
+      }
+   };
 }
 
 export default new AuthController()
