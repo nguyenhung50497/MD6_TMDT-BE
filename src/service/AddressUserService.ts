d8d4c687256b432b33513097d3c979c56b8e74bb
phamthehieu <111071418+phamthehieu@users.noreply.github.com>
2023-03-17 09:47:30 +0700

    merge dev

---
+++ src/service/AddressUserService.ts
@@ -0,0 +1,65 @@
+import {AppDataSource} from "../data-source";
+import {AddressUser} from "../model/addressUser";
+
+class AddressUserService {
+    private addressUserRepository;
+
+    constructor() {
+        this.addressUserRepository = AppDataSource.getRepository(AddressUser)
+    }
+
+    create = async (address) => {
+        try {
+            await this.addressUserRepository.save(address)
+            return 'success'
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    delete = async (idAddress) => {
+        try {
+            let address = await this.addressUserRepository.findOneBy({
+                idAddress: idAddress,
+            });
+            if (!address) {
+                return null;
+            }
+            await this.addressUserRepository.delete({idAddress: idAddress});
+            return 'success'
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    getAll = async (idUser) => {
+        try {
+            let address = await this.addressUserRepository.find({idUser: idUser})
+            return address;
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    update = async (idAddress, newAddress) => {
+        try {
+            let address = await this.addressUserRepository.findOneBy({
+                idAddress: idAddress,
+            });
+            if (!address) {
+                return null;
+            }
+            await this.addressUserRepository.update({idAddress: idAddress}, newAddress);
+            return 'success'
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    findByIdAddress = async (idAddress) => {
+        try {
+            let address = await this.addressUserRepository.findOneBy({idAddress: idAddress,});
+            return address;
+        } catch (e) {
+            console.log(e)
+        }
+    }
+}
+
+export default new AddressUserService()
