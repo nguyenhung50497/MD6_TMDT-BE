8d50dcb1a2743165991a3a02357dd1463dce4abb
ChatNghe <namnt98@gmail.com>
2023-03-21 17:24:55 +0700

    save

--- src/router/cartDetail-router.ts
+++ src/router/cartDetail-router.ts
@@ -4,6 +4,6 @@ import { userAuth } from "../middleware/userAuth";
 import cartDetailController from "../controller/CartDetailController";
 import {searchRouter} from "./search-router";
 export const cartDetailRouter = Router();
-cartDetailRouter.use(auth);
+// cartDetailRouter.use(auth);
 cartDetailRouter.post("/", cartDetailController.createCartDetail);
 cartDetailRouter.get("/stats/sales",cartDetailController.salesStatsCartDetail)
--- src/service/CartDetailService.ts
+++ src/service/CartDetailService.ts
@@ -34,13 +34,28 @@ class CartDetailService {
         if (req.query.week !== undefined && req.query.month !== undefined && req.query.quarter !== undefined && req.query.year !== undefined) {
             for (let i = 0; i < allSalesStats.length; i++) {
                 let day = allSalesStats[i].timePayCart
+                let month = new Date(day).getMonth() + 1;
+                let year = new Date(day).getFullYear()
+                let quarter
+                if(month === 1 || month === 2 || month === 3){
+                    quarter = '1'
+                }
+                if(month === 4 || month === 5 || month === 6){
+                    quarter = '2'
+                }
+                if(month === 7 || month === 8 || month === 9){
+                    quarter = '3'
+                }
+                if(month === 10 || month === 11 || month === 12){
+                    quarter = '4'
+                }
                 let getWeekOfMonth = function (day) {
                     let firstWeekday = new Date(day.getFullYear(), day.getMonth(), 1).getDay();
                     let offsetDate = day.getDate() + firstWeekday - 1;
                     return Math.ceil(offsetDate / 7);
                 }
                 let myDate = new Date(day)
-                if (getWeekOfMonth(myDate) === +req.query.week) {
+                if (getWeekOfMonth(myDate) === +req.query.week && month === +req.query.month && quarter === req.query.quarter  && year === +req.query.year) {
                     salesStats.push(allSalesStats[i])
                 }
             }
@@ -49,7 +64,21 @@ class CartDetailService {
             for (let i = 0; i < allSalesStats.length; i++) {
                 let day = allSalesStats[i].timePayCart
                 let month = new Date(day).getMonth() + 1;
-                if (month === +req.query.month) {
+                let year = new Date(day).getFullYear()
+                let quarter
+                if(month === 1 || month === 2 || month === 3){
+                    quarter = '1'
+                }
+                if(month === 4 || month === 5 || month === 6){
+                    quarter = '2'
+                }
+                if(month === 7 || month === 8 || month === 9){
+                    quarter = '3'
+                }
+                if(month === 10 || month === 11 || month === 12){
+                    quarter = '4'
+                }
+                if (month === +req.query.month &&  quarter === req.query.quarter && year === +req.query.year) {
                     salesStats.push(allSalesStats[i])
                 }
             }
@@ -58,20 +87,21 @@ class CartDetailService {
             for (let i = 0; i < allSalesStats.length; i++) {
                 let day = allSalesStats[i].timePayCart
                 let month = new Date(day).getMonth() + 1;
+                let year = new Date(day).getFullYear()
                 let quarter
                 if(month === 1 || month === 2 || month === 3){
-                    quarter = 1
+                    quarter = '1'
                 }
                 if(month === 4 || month === 5 || month === 6){
-                    quarter = 2
+                    quarter = '2'
                 }
                 if(month === 7 || month === 8 || month === 9){
-                    quarter = 3
+                    quarter = '3'
                 }
                 if(month === 10 || month === 11 || month === 12){
-                    quarter = 4
+                    quarter = '4'
                 }
-                if (quarter === +req.query.quarter) {
+                if (quarter === req.query.quarter && year === +req.query.year) {
                     salesStats.push(allSalesStats[i])
                 }
             }
